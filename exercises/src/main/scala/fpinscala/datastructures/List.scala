e5056c5804b925c76557bf3c43f2533dfbe7895a
lagenorhynchus <ignorantia.juris.non.excusa@gmail.com>
2015-10-26 00:33:19 +0900

    add exercises

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -49,20 +49,70 @@ object List { // `List` companion object. Contains functions for creating and wo
   def product2(ns: List[Double]) =
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
-
+  // Exercise 2
   def tail[A](l: List[A]): List[A] = sys.error("todo")
 
+  // Exercise 3
   def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
 
+  // Exercise 4
   def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
 
+  // Exercise 5
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
 
+  // Exercise 6
   def init[A](l: List[A]): List[A] = sys.error("todo")
 
+  // Exercise 9
   def length[A](l: List[A]): Int = sys.error("todo")
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+  // Exercise 10
+  def foldLeft[A, B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
 
+  // Exercise 11
+  def sum3(ns: List[Int]): Int = sys.error("todo")
+
+  def product3(ns: List[Double]): Double = sys.error("todo")
+
+  // Exercise 12
+  def reverse[A](l: List[A]): List[A] = sys.error("todo")
+
+  // Exercise 13
+  def foldLeft2[A, B](l: List[A], z: B)(f: (B, A) => B): B = sys.error("todo")
+
+  def foldRight2[A, B](l: List[A], z: B)(f: (A, B) => B): B = sys.error("todo")
+
+  // Exercise 14
+  def append2[A](a1: List[A], a2: List[A]): List[A] = sys.error("todo")
+
+  // Exercise 15
+  def concat[A](ls: List[List[A]]): List[A] = sys.error("todo")
+
+  // Exercise 16
+  def add1(ns: List[Int]): List[Int] = sys.error("todo")
+
+  // Exercise 17
+  def doubleToString(ns: List[Double]): List[Double] = sys.error("todo")
+
+  // Exercise 18
   def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
+
+  // Exercise 19
+  def filter[A](l: List[A])(f: A => Boolean): List[A] = sys.error("todo")
+
+  // Exercise 20
+  def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = sys.error("todo")
+
+  // Exercise 21
+  def filter2[A](l: List[A])(f: A => Boolean): List[A] = sys.error("todo")
+
+  // Exercise 22
+  def addEach(a1: List[Int], List[Int]): List[Int] = sys.error("todo")
+
+  // Exercise 23
+  def zipWith[A, B, C](as: List[A], bs: List[B])(f: (A, B) => C): List[C] = sys.error("todo")
+
+  // Exercise 24
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = sys.error("todo")
 }
--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -6,8 +6,12 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
 object Tree {
+  // Exercise 25
 
+  // Exercise 26
 
+  // Exercise 27
 
+  // Exercise 28
 
 }
